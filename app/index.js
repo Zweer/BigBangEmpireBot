b7237d1f237e26243807ba0ab91dfd9eacc68b00
Zweer <n.olivieriachille@gmail.com>
2019-02-18 10:48:37 +0100

    Removing files

--- app/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const config = require('config');
-
-const BBE = require('./lib/bbe');
-const BBET = require('./lib/telegram');
-
-const bbe = new BBE(config.get('bbe'));
-const bbet = new BBET(config.get('telegram'), bbe);
